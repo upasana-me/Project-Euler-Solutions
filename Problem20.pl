e9c5b976f5e6608977e74034eb5b68cd8c7ae641
Upasana Shukla <upasna.shukla@gmail.com>
2012-12-19 02:09:46 +0530

    Documented Problem20

--- Problem20.pl
+++ Problem20.pl
@@ -1,6 +1,9 @@
-# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# n! means n × (n − 1) × ... × 3 × 2 × 1
 
-# What is the sum of the digits of the number 21000?
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
 
 #!/usr/bin/perl
 
@@ -9,7 +12,7 @@ use warnings;
 
 import config;
 
-my ($power50, $limit, $carry, $i, $j) = (1, 50, 0);
+my ($carry, $i, $j) = (0);
 $_ = '0'x1000;
 my @tempResult = split//;
 $tempResult[0] = 1;
